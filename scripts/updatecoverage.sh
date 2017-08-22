9b51e0c7f2493a200ce315993de047bdbc5c268d
Michał Breiter <michalbreiter@gmail.com>
2017-08-22 18:51:53 +0200

    coverage: support for out of source builds

--- scripts/updatecoverage.sh
+++ scripts/updatecoverage.sh
@@ -3,6 +3,7 @@
 # Updates the coverage documentation, and copies it into the appropriate place
 # in the gh-pages-develop branch.
 #   $1 from CMAKE will contain the root directory of cereal
+#   $2 from CMAKE will contain the root directory of build folder
 
 # this requires lcov 1.10 or newer
 
@@ -20,12 +21,22 @@ for f in $COVERAGE_TESTS
 # build coverage output
 tempdir=`mktemp -d`
 
-lcov --capture --directory $1 --output-file coverage.info --no-external
-lcov --remove coverage.info '*/external/*' 'cereal/details/util.hpp' 'sandbox/*' 'unittests/*' -o coverage.info
+lcov --capture --directory $2 --output-file coverage.info --no-external --base $1
+lcov --remove coverage.info "*/external/*" "*/details/util.hpp" "$1/sandbox/*" "$1/unittests/*" -o coverage.info
 genhtml --demangle-cpp coverage.info --output-directory ${tempdir}
 
+echo "output written to ${tempdir}"
+
+pushd $1
 # copy over to gh pages
-git checkout gh-pages-develop
+git checkout gh-pages-develop || {
+    echo "could not switch branch"
+    echo "output written to ${tempdir}"
+    popd
+    exit 1
+    }
+
+popd
 
 rm -rf $1/assets/coverage
 mkdir $1/assets/coverage
